d9c077773c9abbcb6e6e698785a69fb17260d3d8
mladrad <radevmladen80@gmail.com>
2024-05-29 15:03:49 +0300

    small change for better performance

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,42 +1,71 @@
 
+
 import random
 
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
+player_move = ''
+player_choise = ''
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+while player_choise != 'n':
+    while player_move != 'r' or player_move != 'p' or player_move != 's':
+        player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+        print()
 
-if player_move == 'r':
-    player_move = rock
-elif player_move == 'p':
-    player_move = paper
-elif player_move == 's':
-    player_move = scissors
-else:
-    raise SystemExit("Invalid input. Try again...")
+        if player_move == 'r':
+            player_move = rock
+            break
+        elif player_move == 'p':
+            player_move = paper
+            break
+        elif player_move == 's':
+            player_move = scissors
+            break
+        else:
+            print("Invalid input !")
+            player_choise = input("If you want to try again, please enter [y]: ")
+            print()
+            if player_choise == 'y':
+                continue
+            else:
+                print("Thank You for Your time !")
+                raise SystemExit("Have a good day !")
 
-computer_random_number = random.randint(1, 3)
+    computer_random_number = random.randint(1, 3)
 
-computer_move = ""
+    computer_move = ""
 
-if computer_random_number == 1:
-    computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
-print(f"The computer chose {computer_move}.")
+    if computer_random_number == 1:
+        computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
+    print(f"The computer chose {computer_move}.")
+    print()
 
-# Rock beats scissors (the scissors get broken by the rock)
-# Scissors beat paper (the paper gets cut by the scissors)
-# Paper beats rock (the paper covers the rock)
+    # Rock beats scissors (the scissors get broken by the rock)
+    # Scissors beat paper (the paper gets cut by the scissors)
+    # Paper beats rock (the paper covers the rock)
 
-if (computer_move == rock and player_move == scissors) or \
-        (computer_move == scissors and player_move == paper) or \
-        (computer_move == paper and player_move == rock):
-    print("You lose!")
-elif computer_move == player_move:
-    print("Draw!")
-else:
-    print("You win!")
+    if (computer_move == rock and player_move == scissors) or \
+            (computer_move == scissors and player_move == paper) or \
+            (computer_move == paper and player_move == rock):
+        print("You lose!")
+        print()
+    elif computer_move == player_move:
+        print("Draw!")
+        print()
+    else:
+        print("You win!")
+        print()
+
+    player_choise = input("If you want to try again, please enter [y]: ")
+    print()
+    if player_choise == 'y':
+        continue
+    else:
+        print("Thank You for Your time !")
+        raise SystemExit("Have a good day !")
+
